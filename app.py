7320c06f2c3fbddf73ab88407f24402b9b681d38
lazycat0706 <236082202@qq.com>
2020-12-03 16:29:57 +0800

    仿豆瓣小程序v1.009.002

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
+app.config.update({
+    'DEBUG': True,
+    'TEMPLATES_AUTO_RELOAD': True
+})
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
